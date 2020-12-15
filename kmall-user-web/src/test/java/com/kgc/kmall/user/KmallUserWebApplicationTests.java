80d88b5fad5ce4b07266b021d13d574062216092
zhangjiahe_glb <zhangjiahe_glb@kgc.com>
2020-12-16 00:02:09 +0800

    Kmall

---
+++ kmall-user-web/src/test/java/com/kgc/kmall/user/KmallUserWebApplicationTests.java
@@ -0,0 +1,20 @@
+package com.kgc.kmall.user;
+
+import com.kgc.kmall.bean.Member;
+import com.kgc.kmall.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class KmallUserWebApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
