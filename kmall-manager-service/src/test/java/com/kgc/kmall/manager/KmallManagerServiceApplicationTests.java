cbf92699c7f3861314a1472f46f94b1b42372bab
zhangjiahe_glb <zhangjiahe_glb@kgc.com>
2020-12-16 15:29:59 +0800

    kmall-manager-service

---
+++ kmall-manager-service/src/test/java/com/kgc/kmall/manager/KmallManagerServiceApplicationTests.java
@@ -0,0 +1,22 @@
+package com.kgc.kmall.manager;
+
+import com.kgc.kmall.bean.PmsBaseCatalog1;
+import com.kgc.kmall.service.CatalogService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class KmallManagerServiceApplicationTests {
+@Resource
+CatalogService catalogService;
+	@Test
+	void contextLoads() {
+		List<PmsBaseCatalog1> catalog1 = catalogService.getCatalog1();
+		System.out.println(catalog1);
+
+	}
+
+}
