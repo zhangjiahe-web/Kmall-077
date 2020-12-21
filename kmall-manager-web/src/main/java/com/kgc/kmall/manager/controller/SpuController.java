7995457320d716f4f73ca901b98e5eb568d9aa03
zhangjiahe_glb <zhangjiahe_glb@kgc.com>
2020-12-21 18:29:18 +0800

    kmall-manager-web实现fastdfs-client-java上传图片

--- kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
+++ kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
@@ -3,7 +3,13 @@ package com.kgc.kmall.manager.controller;
 import com.kgc.kmall.bean.PmsBaseSaleAttr;
 import com.kgc.kmall.bean.PmsProductInfo;
 import com.kgc.kmall.service.SpuService;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.dubbo.config.annotation.Reference;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,7 +19,8 @@ import java.util.List;
 public class SpuController {
     @Reference
     SpuService spuService;
-
+@Value("${fileServer.url}")
+String fileServer;
     @RequestMapping("/spuList")
     public List<PmsProductInfo> spuList(Long catalog3Id){
         List<PmsProductInfo> infoList = spuService.spuList(catalog3Id);
@@ -28,10 +35,32 @@ public class SpuController {
     public String fileUpload(@RequestParam("file")MultipartFile file){
         //文件上传
         //返回文件上传后的路径
-        return "https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+        try {
+            String conFile = this.getClass().getResource("/tracker.conf").getFile();
+            ClientGlobal.init(conFile);
+            TrackerClient trackerClient=new TrackerClient();
+            TrackerServer trackerServer=trackerClient.getTrackerServer();
+            StorageClient storageClient=new StorageClient(trackerServer,null);
+            String originalFilename = file.getOriginalFilename();
+            String extName = FilenameUtils.getExtension(originalFilename);
+            String[] upload_file = storageClient.upload_file(file.getBytes(), extName, null);
+            String path=fileServer;
+            for (int i = 0; i < upload_file.length; i++) {
+                String s = upload_file[i];
+                System.out.println("s = " + s);
+                path+="/"+s;
+            }
+            System.out.println(path);
+            return path;
+
+        }catch (Exception ex){
+return null;
+        }
+
     }
     @RequestMapping("/saveSpuInfo")
     public String saveSpuInfo(@RequestBody  PmsProductInfo pmsProductInfo){
+
         return "success";
     }
 }
--- kmall-manager-web/src/test/java/com/kgc/kmall/manager/KmallManagerWebApplicationTests.java
+++ kmall-manager-web/src/test/java/com/kgc/kmall/manager/KmallManagerWebApplicationTests.java
@@ -1,5 +1,9 @@
 package com.kgc.kmall.manager;
 
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,26 @@ class KmallManagerWebApplicationTests {
 
 	@Test
 	void contextLoads() {
+		try {
+			String file = this.getClass().getResource("/tracker.conf").getFile();
+			ClientGlobal.init(file);
+			TrackerClient trackerClient=new TrackerClient();
+			TrackerServer trackerServer=trackerClient.getTrackerServer();
+			StorageClient storageClient=new StorageClient(trackerServer,null);
+			String orginalFilename="C:\\Users\\张家和\\Desktop\\图片和视频\\蔚蓝网图片\\9539952.jpg";
+			String[] upload_file = storageClient.upload_file(orginalFilename, "jpg", null);
+			String path="http://192.168.206.129";
+			for (int i = 0; i < upload_file.length; i++) {
+				String s = upload_file[i];
+				System.out.println("s = " + s);
+				path+="/"+s;
+			}
+			System.out.println(path);
+		}catch (Exception ex){
+
+		}
+
+
 	}
 
 }
